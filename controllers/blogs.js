fd62d492bd845cbe180369f1c34eb8fe95c274c1
roossh <roossh@users.noreply.github.com>
2019-02-12 01:14:36 +0200

    Initial commit

---
+++ controllers/blogs.js
@@ -0,0 +1,32 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs.map(blog => blog.toJSON()))
+  })
+})
+
+blogsRouter.get('/:id', (request, response) => {
+  Blog.findById(request.params.id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog.toJSON())
+      } else {
+        response.status(404).end()
+      }
+    })
+    /*.catch(error => next(error))*/
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
