cdff75bc466aefc1beff0ab30d5602316b1d3e45
roossh <roossh@users.noreply.github.com>
2019-02-15 22:37:28 +0200

    4.16

--- controllers/users.js
+++ controllers/users.js
@@ -12,6 +12,14 @@ usersRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
 
+    if (body.password.length < 3) {
+      return response.status(400).json({error: 'Password must be at least 3 characters'})
+    }
+
+    if (body.username.length < 3) {
+      return response.status(400).json({error: 'Username must be at least 3 characters'})
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
--- models/user.js
+++ models/user.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        unique: true,
+        minLength: 3,
+        required: true
+    },
     name: String,
     passwordHash: String,
     blogs: [
@@ -12,9 +18,11 @@ const userSchema = mongoose.Schema({
     ]
 })
 
+userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id
+        returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
         delete returnedObject.passwordHash
